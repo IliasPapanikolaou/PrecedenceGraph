d8c67077777e2489fa0ee2bebf158a680689103b
dlnioanna <dln.ioanna@gmail.com>
2021-02-10 03:44:58 +0200

    Block added, filtering genesis block from Process list

--- src/main/java/com/unipi/precedence_graph/PrecedenceGraph.java
+++ src/main/java/com/unipi/precedence_graph/PrecedenceGraph.java
@@ -1,12 +1,17 @@
 package com.unipi.precedence_graph;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class PrecedenceGraph {
+    public static int prefix = 2;
+    public static List<Block> blockChain = new ArrayList<>();
 
     public static void main(String[] args) {
 
@@ -15,20 +20,29 @@ public class PrecedenceGraph {
         System.out.println("================== Summarize ====================");
         int countGenesis = 0;
         int countSecondary = 0;
-        for (Process p : processes){
+        for (Process p : processes) {
             if (p.isGenesisProcess()) countGenesis++;
             else countSecondary++;
         }
 
-        System.out.println("Number of genesis processes: " +countGenesis);
-        System.out.println("Number of secondary processes: " +countSecondary);
+        System.out.println("Number of genesis processes: " + countGenesis);
+        System.out.println("Number of secondary processes: " + countSecondary);
 
-        for (Process p: processes){
-            if (p.getWaitTime() != 0) System.out.println(p.getName() +" has to wait for " +p.getWaitTime() +"ms");
+        for (Process p : processes) {
+            if (p.getWaitTime() != 0) System.out.println(p.getName() + " has to wait for " + p.getWaitTime() + "ms");
         }
 
         System.out.println("============== Starting Threads =================");
+        if (countGenesis > 0) {
+           Block genesisBlock = new Block("0",processes,new Date().getTime());
+           genesisBlock.executeProcces();
+           genesisBlock.mineBlock(prefix);
+            blockChain.add(genesisBlock);
+            System.out.println("Node:"+(blockChain.size()-1)+" created");
 
+        }else{
+            System.out.println("You must define at least one independent starting thread.");
+        }
 
 
     }
--- src/main/java/com/unipi/precedence_graph/Process.java
+++ src/main/java/com/unipi/precedence_graph/Process.java
@@ -7,12 +7,14 @@ public class Process implements Runnable {
 
     private final String name;
     private final boolean isGenesis;
+    private boolean isCompleted;
     private int waitTime;
     private List<String> depedencies;
 
     public Process(Builder builder){
         this.name = builder.name;
         this.isGenesis = builder.isGenesis;
+        this.isCompleted = false;
         this.depedencies = new ArrayList<>();
     }
 
@@ -32,6 +34,10 @@ public class Process implements Runnable {
         return this.isGenesis;
     }
 
+    public boolean isCompleted(){
+        return this.isCompleted;
+    }
+
     public String getName(){
         return this.name;
     }
@@ -48,6 +54,8 @@ public class Process implements Runnable {
     }
 
     public void addDependencies(String process) {this.depedencies.add(process);}
+
+    public void setCompleted(boolean isCompleted) {this.isCompleted=isCompleted;}
 
     //Builder
     public static class Builder{
